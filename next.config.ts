ce41dc5cf86e0d0c37fe63f5e5c1fdda6265ebac
KingBuldoze <abhaynamb02@gmail.com>
2026-05-11 13:26:37 +0530

    Deployment: configure Next.js static export and GitHub Actions workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/MY_Portfolio',
+  assetPrefix: '/MY_Portfolio',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
